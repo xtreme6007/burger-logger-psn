02873e277cff03a34a2300c4aed387596bbc2877
xtreme6007 <prestonnichols96@gmail.com>
2020-09-26 22:34:26 -0500

    intial commit and adding in files and setting up file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swish', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconater', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('PB&J', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan', false);
